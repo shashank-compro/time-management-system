35a5527af895f9bcf340ad449e8767c420e970f7
parul <parul.rawat@comprotechnologies.com>
2019-09-16 17:21:55 +0530

    client side code for time entries

--- Server/controllers/time-entry.controller.js
+++ Server/controllers/time-entry.controller.js
@@ -4,13 +4,117 @@
  *
  * @class timeEntriesController
  */
+const mongoose = require('mongoose');
+const timeEntryModel = require('../models/time-entry.model');
+const dataLayer = require('../datalayer/time-entry.datalayer')
+const moment = require('moment');
+
+
 class timeEntriesController {
 
-    static getTimeEntriesByUserId (req, res) {}
+    //  GET 
 
-    static createTimeEntry (req, res) {}
+    static async getTimeEntriesByUserId(req, res) {
+        let timeEntries;
 
-    static updateTimeEntryById (req, res) {}
-   
+        const obj = {
+            "userId": req.query.user,
+            "limit": req.query.limit
+        }
+
+        try {
+            timeEntries = await dataLayer.getTimeEntriesByUserId(obj);
+        } catch (e) {
+            res.status(400).send({
+                errorCode: e.errorCode,
+                dbMessage: e.dbMessage
+            });
+        }
+
+        res.status(200).json(timeEntries);
+
+    }
+
+    //POST
+
+    static async createTimeEntry(req, res) {
+        let entry;
+        var dateAndTimeObj = {
+            date: req.body.date,
+            timeIn: req.body.timeIn,
+            timeOut: req.body.timeOut
+        }
+        //Formatting of date and time
+        let ISODateAndTimeObj = timeEntriesController.toISOFormat(dateAndTimeObj);
+
+        let newTimeEntry = new timeEntryModel({
+            userId: mongoose.Types.ObjectId(req.body.userId), //need to check token from login page
+            date: ISODateAndTimeObj.date,
+            timeIn: ISODateAndTimeObj.timeIn,
+            timeOut: ISODateAndTimeObj.timeOut,
+            onLeave: req.body.onLeave
+        });
+
+        try {
+            entry = await dataLayer.createTimeEntry(newTimeEntry);
+
+            // //save entry ID - WILL BE FETCHED FROM URL IN FUTURE
+            // timeEntriesController.currentEntryID = entry._id;
+        } catch (e) {
+            res.status(400).send({
+                errorCode: e.errorCode,
+                dbMessage: e.dbMessage
+            });
+        }
+        res.status(200).json(entry);
+    }
+
+    static toISOFormat(dateAndTimeObj) {
+
+        //Set date
+        var dateObj = dateAndTimeObj.date.split('/');
+        let date = moment(new Date(dateObj[0], dateObj[1] - 1, dateObj[2])).format('YYYY-MM-DD');
+        console.log('s', date)
+
+        //Making ISO standard time stamp
+        let a = date;
+
+        let timeIn = moment(`${a} ${dateAndTimeObj.timeIn}`).toISOString();
+        let timeOut = moment(`${a} ${dateAndTimeObj.timeOut}`).toISOString();
+
+        return {
+            "date": date,
+            "timeIn": timeIn,
+            "timeOut": timeOut
+        };
+    }
+
+    //PUT
+
+    static async updateTimeEntryById(req, res) {
+
+        let updatedTimeEntry;
+        let ISODateAndTimeObj = timeEntriesController.toISOFormat(req.body);
+
+        var obj = {
+            userId: mongoose.Types.ObjectId(req.body.userId),
+            date: ISODateAndTimeObj.date,
+            timeIn: ISODateAndTimeObj.timeIn,
+            timeOut: ISODateAndTimeObj.timeOut,
+            onLeave: req.body.onLeave,
+            entryID: req.params.timeEntryId
+        }
+        try {
+            updatedTimeEntry = await dataLayer.updateTimeEntryById(obj);
+
+        } catch (e) {
+            res.status(400).send({
+                errorCode: e.errorCode,
+                dbMessage: e.dbMessage
+            });
+        }
+        res.status(200).json(updatedTimeEntry);
+    }
+
 }
 module.exports = timeEntriesController;
--- Server/datalayer/time-entry.datalayer.js
+++ Server/datalayer/time-entry.datalayer.js
@@ -1,3 +1,4 @@
+const timeentries = require('../models/time-entry.model');
 
 /*
  * @class timeEntriesDatalayer
@@ -5,8 +6,52 @@
 class timeEntriesDatalayer {
 
     /**
-    * Initializes the datalayer
-    */
+     * Initializes the datalayer
+     */
+    static getTimeEntriesByUserId(obj) {
+
+        let param = {
+            "userId" : obj.userId,
+            "limit" : parseInt(obj.limit)
+        }
+        let sortOrder = {
+            '_id': 1
+        };
+        console.log(param,param.userId,param.limit)
+
+        return timeentries
+            .find({"userId" : param.userId}, null, {limit: param.limit})
+            .sort(sortOrder)
+            .exec()
+            .catch((err) => {
+            if (err) throw new Error(`Could'nt fetch data user with ID ${userId}`);
+        });
+    }
+
+    static createTimeEntry(newTimeEntry) {
+        return newTimeEntry.save().catch((err) => {
+            if (err) throw new Error (`Could not add leave. Error stack - ${err}`);
+            else console.log("Entry submitted: ", data);
+        });
+    }
+
+    static updateTimeEntryById(obj) {
+
+        const filter = {
+            _id: obj.entryID
+        };
+        const update = {
+            timeIn: obj.timeIn,
+            timeOut: obj.timeOut
+        };
+        // mongoose.set('useFindAndModify', false);
+        return timeentries.findOneAndUpdate(filter, update, {
+            new: true
+        }).exec().catch( (err, entry) => {
+            if (err) throw new Error('Could not update Time entry');
+            return entry;
+        });
+    }
 
 }
 
