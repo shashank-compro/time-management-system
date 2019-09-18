6d75eaad58096f782f599d3ceb1a3a4c9ccd5dc1
shashank <shashank.jha@comprotechnologies.com>
2019-09-18 16:23:41 +0530

    Leaves datalayer and contoller setup

--- Server/controllers/leave.controller.js
+++ Server/controllers/leave.controller.js
@@ -1,4 +1,5 @@
 "use strict";
+const leavesDataLayer = require('../datalayer/leave.datalayer');
 /**
  * Class representing leaves controller
  *
@@ -6,13 +7,52 @@
  */
 class leavesController {
 
-    static getLeavesByUserId (req, res) {}
+    static async getLeavesByUserId (req, res) {
+        const userId = req.query.user;
+        let leavesList;
+        try {
+            leavesList = await leavesDataLayer.getLeavesByUserId(userId);
+        } catch (err) {
+            console.log(err.message);
+            return res.status(400);
+        }
+        return res.status(200).json(leavesList);
+    }
 
-    static createLeave (req, res) {}
+    static async createLeave (req, res) {
+        let createdLeave;
+        try {
+            createdLeave = await leavesDataLayer.createLeave(req.body);
+        } catch(err) {
+            console.log(err.message);
+            return res.status(400);
+        }
+        return res.status(201).json(createdLeave);
+    }
 
-    static updateLeaveById (req, res) {}
+    static async updateLeaveById (req, res) {
+        const leaveId = req.params.leaveId;
+        let updatedLeave;
+        try {
+            updatedLeave = await leavesDataLayer.updateLeaveById(leaveId, req.body);
+        } catch(err) {
+            console.log(err.message);
+            return res.status(400);
+        }
+        return res.status(200).json(updatedLeave);
+    }
 
-    static deleteLeaveById (req, res) {}
+    static async deleteLeaveById (req, res) {
+        const leaveId = req.params.leaveId;
+        let deletedLeave;
+        try {
+            deletedLeave = await leavesDataLayer.deleteLeaveById(leaveId);
+        } catch(err) {
+            console.log(err.message);
+            return res.status(400);
+        }
+        res.status(200).json(deletedLeave);
+    }
   
 }
 module.exports = leavesController;
