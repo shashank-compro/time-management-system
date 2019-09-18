a6a690e4265dcc524dcca5e0fc8535497752bba0
shashank <shashank.jha@comprotechnologies.com>
2019-09-18 16:23:41 +0530

    Leaves datalayer and contoller setup

--- Server/datalayer/leave.datalayer.js
+++ Server/datalayer/leave.datalayer.js
@@ -1,13 +1,43 @@
+const mongoose = require('mongoose');
+const leavesModel = require('../models/leave.model');
 
-/*
- * @class leavesDatalayer
- */
-class leavesDatalayer {
+class leavesDataLayer {
 
-    /**
-    * Initializes the datalayer
-    */
+    static getLeavesByUserId (userId) {
+        return leavesModel.find({'userId': mongoose.Types.ObjectId(userId)}).exec().catch((err) => {
+            throw new Error (`Could not retrieve leaves. Error message - ${err.message}`);
+        });
+    }
 
+    static createLeave (reqObj) {
+        let {endDate, reason, startDate} = reqObj;
+        const newLeave = new leavesModel({
+            startDate: startDate,
+            endDate: endDate,
+            reason: reason,
+            userId: mongoose.Types.ObjectId(reqObj.userId)
+        });
+        return newLeave.save().catch((err) => {
+            throw new Error (`Could not create leave. Error message - ${err.message}`);
+        });
+    }
+
+    static updateLeaveById (leaveId, reqObj) {
+        let {endDate, reason, startDate} = reqObj;
+        let updatedLeave = {
+            startDate: startDate,
+            endDate: endDate,
+            reason: reason
+        };
+        return leavesModel.findByIdAndUpdate(leaveId, updatedLeave).exec().catch((err) => {
+            throw new Error (`Could not update leave. Error message - ${err.message}`);
+        });
+    }
+
+    static deleteLeaveById (leaveId) {
+        return leavesModel.findByIdAndRemove(leaveId).exec().catch((err) => {
+            throw new Error (`Could not delete leave. Error message - ${err.message}`);
+        });
+    }
 }
-
-module.exports = leavesDatalayer;
+module.exports = leavesDataLayer;
