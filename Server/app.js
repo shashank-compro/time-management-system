c7d744f7b5519ec6b9b4609982b16d4c55feb492
shashank <shashank.jha@comprotechnologies.com>
2019-09-19 16:26:12 +0530

    Adding cors module

--- Server/app.js
+++ Server/app.js
@@ -3,9 +3,22 @@ const bodyParser = require('body-parser');
 const config = require('./config/default');
 const DataLayerFactory = require('./datalayer/factory.datalayer');
 const routes = require('./config/routes');
+var cors = require('cors');
+
+const whitelist = ['http://localhost:4200'];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }
+  } else {
+    corsOptions = { origin: false }
+  }
+  callback(null, corsOptions)
+}
 
 const app = express();
 app.use(bodyParser.json({limit: '1mb'}));
+app.use(cors(corsOptionsDelegate));
 
 app.use('/api/v1', routes);
 
